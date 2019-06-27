a26282368bb588f4a1157a5dcc055c34423f053e
Prof Game <kadomony@gmail.com>
2019-06-27 16:47:29 +0200

    GUI test scene and Standard Assets

---
+++ Game04-Unity-Sandbox/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void StartGame() {
+        SceneManager.LoadScene("AnimationTest");
+    }
+
+    public void DisplaySliderValue(float value) {
+        Debug.Log("Slider value : " + value.ToString());
+    }
+
+}
