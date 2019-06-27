1560a7ec43c9dd343b8ed689b90a73eebfbb83c6
Prof Game <kadomony@gmail.com>
2019-06-27 13:25:08 +0200

    Testing the animation and animator

---
+++ Game04-Unity-Sandbox/Assets/Scripts/CubeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+
+    private Animator anim;
+
+    void Start() {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            anim.SetBool("startMoving", true);
+        }
+    }
+}
